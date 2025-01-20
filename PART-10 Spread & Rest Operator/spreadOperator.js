7ad3a513e786ada519a59681e6b42464f3a8dc2a
Student ApnaCollege <sujitsaran16@gmail.com>
2025-01-20 16:37:10 +0530

    All CSS Files are removed

---
+++ PART-10 Spread & Rest Operator/spreadOperator.js
@@ -0,0 +1,6 @@
+// Spread Operator
+// ----------------
+// The spread operator(...) is a new addition to the set of operators in Javascript ES6. 
+// It takes in an iterable (e.g array, string, object) and expands it into individual elements. 
+// The spread operator is commonly used to make shallow copies of JS Objects. 
+// Using this operator makes the code consise and anhances its readability
